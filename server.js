ddcdf4c9a77b31ac83fd4bff176a6f7efcf48aea
Hurash <ashleyhurst930@gmail.com>
2020-05-10 17:52:51 +0100

    Added React App

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
-app.get('/', (req, res) => res.send('Hello ashley'));
+// Connect Database
+connectDB();
+
+//init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.json({ msg: 'Hello ashley' }));
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
