5e2eb90bc5a9d79d842e625c97c213da32eec5d4
Hurash <ashleyhurst930@gmail.com>
2020-05-12 20:04:05 +0100

    Add React

---
+++ models/Contact.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = new mongoose.Schema({
+  //Each user has their own individual contacts
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    default: 'personal',
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('contact', ContactSchema);
